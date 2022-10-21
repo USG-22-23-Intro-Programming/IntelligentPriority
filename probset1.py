cb38c1439b774822c4a62c4dac28d863a208b605
Cathy Kintner <cathy_kintner@m018386.aw.org>
2022-10-21 12:01:51 -0700

    problem set #1 python

---
+++ probset1.py
@@ -0,0 +1,59 @@
+
+#1
+'''def main():
+    x = input("Please enter your name")
+    print(x)
+    print("Hello " +x)
+
+
+if __name__== "__main__":
+    main()'''
+
+#2
+'''def main():
+   x = 2
+   print(x)
+   y = 10
+   print(y)
+   if x % y == 0:
+       print("multiples")
+   else:
+       print("not multiples")
+
+
+if __name__== "__main__":
+    main()'''
+
+
+#3
+def main():
+   pal=input(("Enter palindrome:"))
+      
+   if(pal[0]==pal[len(pal)-1]):
+      print("is a palindrome")
+   else:
+      print("is not palindrome")
+
+
+if __name__== "__main__":
+    main()
+
+
+
+#considine example
+'''length = len(pal)
+   for i in range(length):
+      if pal[i] != pal[length - 1 - i]:
+         return False
+      
+   return True'''
+
+
+
+
+
+
+
+
+
+
